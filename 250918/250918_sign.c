9ee85f02c3ad766e77d320d1dce6f052b0b7b710
ChoiTaeKit <chlxodnd97@gmail.com>
2025-09-18 10:37:56 +0900

    9월18일추가1-1

---
+++ 250918/250918_sign.c
@@ -0,0 +1,17 @@
+// 읽어온 정수값의 양수, 음수, 0 판별
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("정수: ");
+    scanf("%d", &n);
+
+    if (n>0) {
+        printf("양수입니다.\n");
+    } else if (n<0) {
+        printf("음수입니다.\n");
+    } else
+        printf("0입니다.\n");
+    return 0;
+}
